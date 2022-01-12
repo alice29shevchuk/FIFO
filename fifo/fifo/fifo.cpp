c2c26cea848f704d4a0fa59daf7cbc12d8a63d65
Алиса Шевчук <alisa000077@gmail.com>
2022-01-12 15:49:11 +0200

    доделанный алгоритм фифо с удалением

--- fifo/fifo/fifo.cpp
+++ fifo/fifo/fifo.cpp
@@ -9,17 +9,23 @@ struct Node
     }
     Node(Node*node)
     {
-        next = node->next;
+        next = NULL;
         value = node->value;
     }
 };
 class FIFO
 {
     Node* head;
+    int size;
 public:
     explicit FIFO()
     {
         this->head = NULL;
+        this->size = 0;
+    }
+    int getSize()
+    {
+        return this->size;
     }
     void Add(int value)
     {
@@ -40,42 +46,64 @@ public:
             iter->next->value = value;
             iter->next->next = NULL;
         }
+        size++;
     }
     void Show()
     {
         Node* tmp = this->head;
-        while (tmp!= NULL)
+        if (size != 0)
         {
-            std::cout <<"ADRESS:\t"<< tmp << "\t" << tmp->value << "\tNEXT:\t" << tmp->next << "\n";
-            tmp = tmp->next;
-        }
-    }
-    Node* remove()
-    {
-        Node* iter = this->head;
-        if (iter!= NULL)
-        {
-            Node* fordel = new Node(iter);
-            iter = iter->next;
-            return fordel;
+            while (tmp != NULL)
+            {
+                std::cout << "ADRESS:\t" << tmp << "\t" << tmp->value << "\tNEXT:\t" << tmp->next << "\n";
+                tmp = tmp->next;
+            }
         }
         else
         {
             std::cout << "Список пуст!\n";
         }
-        delete iter;
+    }
+    Node* remove()
+    {
+        if (size > 0)
+        {
+            Node* iter = this->head;
+            Node* fordel = new Node(iter);
+            if (iter != NULL)
+            {
+                iter = this->head;
+                this->head = this->head->next;
+                delete iter;
+                size--;
+            }
+            return fordel;
+        } 
     };
 };
 int main()
 {
+    setlocale(0, "");
     FIFO fifo;
     fifo.Add(1);
     fifo.Add(2);
     fifo.Add(3);
+    fifo.Add(4);
     fifo.Show();
     std::cout << "\n";
-    Node* last = fifo.remove();
-    std::cout <<"NEW ADRESS:\t"<< last << "\t" << last->value << "\tNEXT:\t"<<last->next<<"\n";
-    Node* last2 = fifo.remove();
-    std::cout << "NEW ADRESS:\t" << last2 << "\t" << last2->value << "\tNEXT:\t" << last2->next << "\n";
+    do
+    {
+        Node* last = fifo.remove();
+        std::cout << "NEW ADRESS:\t" << last << "\t" << last->value << "\tNEXT:\t" << last->next << "\n\n";
+        //Node* last2 = fifo.remove();
+        //std::cout << "NEW ADRESS:\t" << last2 << "\t" << last2->value << "\tNEXT:\t" << last2->next << "\n\n";
+        //Node* last3 = fifo.remove();
+        //std::cout << "NEW ADRESS:\t" << last3 << "\t" << last3->value << "\tNEXT:\t" << last3->next << "\n\n";
+        //Node* last4 = fifo.remove();
+        //std::cout << "NEW ADRESS:\t" << last4 << "\t" << last4->value << "\tNEXT:\t" << last4->next << "\n\n";
+        //Node* last5 = fifo.remove();
+        //std::cout << "NEW ADRESS:\t" << last5 << "\t" << last5->value << "\tNEXT:\t" << last5->next << "\n\n";
+        //fifo.Show();
+    } while (fifo.getSize() > 0);
+    fifo.Show();
 }
